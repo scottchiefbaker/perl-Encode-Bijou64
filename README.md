3824357641237f753b7221174ba850aa45b13ff3
Scott Baker <scott@perturb.org>
2026-05-29 14:56:55 -0700

    Add the number of bytes required

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Encode::Bijou64 - Encode and decode Bijou64 integers
 ```perl
 use Encode::Bijou64;
 
-my $bytes = encode_bijou64(123456);
-my $num   = decode_bijou64($bytes);
+my $bytes = encode_bijou64(123456); # 0xfa00e048
+my $num   = decode_bijou64($bytes); # 123456
 ```
 
 ## Description
@@ -18,6 +18,24 @@ integer encoding format described by Ink & Switch.
 
 Small integers occupy fewer bytes while preserving
 efficient decoding.
+
+### Bytes Needed
+
+Encoding integers will result in a string of this many bytes
+
+```
+Integer Range                   Total Size
+------------------------------------------
+< 247                           1 byte
+< 503                           2 bytes
+< 66,039                        3 bytes
+< 16,843,255                    4 bytes
+< 4,311,810,551                 5 bytes
+< 1,103,823,438,327             6 bytes
+< 282,578,800,148,983           7 bytes
+< 72,340,172,838,076,919        8 bytes
+< 18,446,744,073,709,551,615    9 bytes
+```
 
 ## Functions
 
--- lib/Encode/Bijou64.pm
+++ lib/Encode/Bijou64.pm
@@ -119,8 +119,8 @@ Encode::Bijou64 - Encode and decode Bijou64 integers
 
   use Encode::Bijou64;
 
-  my $bytes = encode_bijou64(123456);
-  my $num   = decode_bijou64($bytes);
+  my $bytes = encode_bijou64(123456); # 0xfa00e048
+  my $num   = decode_bijou64($bytes); # 123456
 
 =head1 DESCRIPTION
 
@@ -129,6 +129,22 @@ integer encoding format described by Ink & Switch.
 
 Small integers occupy fewer bytes while preserving
 efficient decoding.
+
+=head2 BYTES NEEDED
+
+Encoding integers will result in a string of this many bytes
+
+  Integer Range                   Total Size
+  ------------------------------------------
+  < 247                           1 byte
+  < 503                           2 bytes
+  < 66,039                        3 bytes
+  < 16,843,255                    4 bytes
+  < 4,311,810,551                 5 bytes
+  < 1,103,823,438,327             6 bytes
+  < 282,578,800,148,983           7 bytes
+  < 72,340,172,838,076,919        8 bytes
+  < 18,446,744,073,709,551,615    9 bytes
 
 =head1 FUNCTIONS
 
