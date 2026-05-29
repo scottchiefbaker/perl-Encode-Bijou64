f8f2d95752c08364ac2a4c5cc576047f0c319538
Scott Baker <scott@perturb.org>
2026-05-29 15:01:49 -0700

    Update license to GPL-3

--- README.md
+++ README.md
@@ -57,4 +57,7 @@ Scott Baker
 
 ## License
 
-Same terms as Perl itself.
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
--- lib/Encode/Bijou64.pm
+++ lib/Encode/Bijou64.pm
@@ -166,7 +166,10 @@ Scott Baker
 
 =head1 LICENSE
 
-Same terms as Perl itself.
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
 
 =cut
 
